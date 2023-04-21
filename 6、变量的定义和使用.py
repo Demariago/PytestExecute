f326aecf6ccad7ef5a0a5acc28634227a56585ff
myl <997897692@qq.com>
2023-04-21 11:08:33 +0800

    1

---
+++ 6、变量的定义和使用.py
@@ -0,0 +1,12 @@
+# demaria
+# 时间:2023/4/21 10:58
+#变量：可以改变的量
+name ='maliya'
+# name 变量名
+# = 赋值运算符
+# maliya 值
+
+#变量由三部分构成，标识，类型，值；可以用id，type，print获取，实例如下
+print('值',name)
+print('标识',id(name))
+print('类型',type(name))
