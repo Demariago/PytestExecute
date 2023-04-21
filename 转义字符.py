d60b84a7ed131bd8b1000e89a0c31ab85e727efd
myl <997897692@qq.com>
2023-04-21 10:03:53 +0800

    1

---
+++ 转义字符.py
@@ -0,0 +1,16 @@
+# demaria
+# 时间:2023/4/20 16:19
+#反斜杠的使用
+
+#\n换行  newline
+print('hello\nworld')
+#\t增加四个空格的位置  tab
+print('hello\tworld')
+print('helloooo\tworld')#解释制表符的含义,每四个
+print('he\tworld')
+#r  return回车 将光标定位到该行初始位置
+print('hello\rworld')
+#\b  退一个格的含义
+print('hello\bworld')
+#想要输出单引号呢，用反斜杠标记为正常内容，非边界
+print('老师说:\'大家好\'')
