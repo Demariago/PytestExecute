a804cb88d17deafe04beb4a8f7d5348dad173b26
myl <997897692@qq.com>
2023-04-22 20:54:48 +0800

    1

--- 5、变量的定义和使用.py
+++ 5、变量的定义和使用.py
@@ -1,5 +1,6 @@
 # demaria
 # 时间:2023/4/21 10:58
+
 #变量：可以改变的量
 name ='maliya'
 # name 变量名
@@ -8,6 +9,6 @@ name ='maliya'
 
 #变量由三部分构成，标识，类型，值；可以用id，type，print获取，实例如下
 print('值',name)
-print('标识',id(name))
+print('标识',id(name))#内存标记
 print('类型',type(name))
 #如上有助于理解内存的概念  id+type+value
