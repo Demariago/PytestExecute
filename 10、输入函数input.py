945f45dab67a80ad6cab2d5a5156d121d8eb4a40
myl <997897692@qq.com>
2023-04-23 10:41:21 +0800

    1

--- 10、输入函数input.py
+++ 10、输入函数input.py
@@ -1,19 +1,19 @@
 # demaria
 # 时间:2023/4/21 17:39
-
+#例子如下
 #present=input('你想要啥呢？')
 #print(present,type(present))
-
-
 #从键盘输入两个整数，求和（注意input默认是string类型哦）
 n1=input('第一个数字')
 n2=input('第二个数字')
 print('n1+n2的和(转义前)是:',n1+n2,type(n1),type(n2))
-print('n1+n2的和(转义后)是:',int(n1)+int(n2),type(int(n1)),type(int(n2))
+print('n1+n2的和(转义后)是:',int(n1)+int(n2),type(int(n1)),type(int(n2)))
 
-#或者这么写，利用变量的多次赋值
-n1=input('第一个')
-n1=int(n1)
-n2=input('第二个')
-n2=input(n2)
-print(n1+n2)
+#或者
+#从键盘录入两个整数，计算两个整数的和
+#a = int(input('请输入一个加数：'))
+#b = int(input('请输入另一个加数：'))
+#print(type(a),type(b))
+#print(a+b)
+
+
