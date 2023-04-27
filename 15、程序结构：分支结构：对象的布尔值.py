c30fd7acd2033fdf0fc01fcb9e289ec9b3c5cca9
myl <997897692@qq.com>
2023-04-27 10:33:27 +0800

    1

---
+++ 15、程序结构：分支结构：对象的布尔值.py
@@ -0,0 +1,26 @@
+# demaria
+# 时间:2023/4/27 9:48
+
+#在分支结构开始前，先讲讲对象的布尔值
+#所有对象都有布尔值（即True和False），使用内置函数bool()；就像id(),type()一样
+#以下的bool值都是False
+print('-----以下的bool值均为False，其他的对象布尔值均为True')
+print(bool(False))
+print(bool(0))
+print(bool(0.0))
+print(bool(None))
+print(bool(''))
+print(bool(""))
+print(bool([]))#空列表
+print(bool(list()))#空列表
+print(bool(()))#空元组
+print(bool(tuple()))#空元组
+print(bool(dict()))#空字典
+print(bool({}))#空字典
+print(bool(set()))#空集合
+
+
+print('-----其他的对象的布尔值均为True-----')
+print(bool(18))
+print(bool(True))
+print(bool('helloworld'))
