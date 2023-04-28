b543e9edd6a49e117c5f0889a568eb0acb770d10
myl <997897692@qq.com>
2023-04-28 21:45:18 +0800

    1

---
+++ 29、循环和中断：嵌套循环.py
@@ -0,0 +1,30 @@
+# demaria
+# 时间:2023/4/28 21:10
+
+#用*打印一个三行四列的矩形
+for i in  range(1,4):
+    for j in range(1,5):
+        print('*',end='\t')
+    else:
+        print()
+#和下面的代码做个区分：注意else的位置
+print('区分的代码注意else的位置')
+for i in  range(1,4):
+    for j in range(1,5):
+        print('*',end='\t')
+else:
+    print()
+
+#打印一个9*9的直角三角形
+print('9*9的直角三角形')
+for i in range(1,10):
+    for j in range(1,i+1):
+        print('*',end='\t')
+    print()
+
+#接9*9的三角形，输出99乘法表
+print('9*9的直角三角形')
+for i in range(1,10):
+    for j in range(1,i+1):
+        print(i,'*',j,'=',i*j,end='\t')
+    print()
