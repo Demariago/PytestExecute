89499c2742370cfaa9cee2ebea6045ef273e660a
mayuanli <mayuanli@enn.cn>
2023-09-04 15:06:28 +0800

    1

--- demo111.py
+++ demo111.py
@@ -1,18 +1,38 @@
 # demaria
 # 时间:2023/8/29 18:27
-from time import sleep
+from time import perf_counter
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from ulib.FileUtil import FileUtil
-def get_time():
-service_chrome_driver = Service(executable_path=f"{FileUtil.get_project_path()}/drivers/chromedriver")
-driver = webdriver.Chrome(service=service_chrome_driver)
-driver.implicitly_wait(5)
-driver.get("https://www.baidu.com")
-driver.maximize_window()
-sleep(3)
-time = driver.execute_script("return performance.timing.loadEventStart-performance.timing.fetchStart")
-print(f"页面加载时间： {time}")
-driver.quit()
-if __name__ == '__main__':
-get_time()
+
+
+def get_page_load_time(url):
+    # 创建 Chrome WebDriver
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")  # 设置无界面模式
+    driver = webdriver.Chrome(options=options)
+
+    try:
+        # 记录开始加载时间
+        start_time = perf_counter()
+
+        # 打开页面
+        driver.get(url)
+
+        # 等待页面加载完成
+        driver.implicitly_wait(10)  # 设置隐式等待时间，单位为秒
+
+        # 计算页面加载时间
+        end_time = perf_counter()
+        load_time = end_time - start_time
+
+        return load_time
+
+    finally:
+        # 关闭 WebDriver
+        driver.quit()
+
+
+# 测试页面的 URL
+url = "https://www.baidu.com"  # 替换为您要测试的页面 URL
+load_time = get_page_load_time(url)
+
+print(f"页面加载时间：{load_time} 秒")
